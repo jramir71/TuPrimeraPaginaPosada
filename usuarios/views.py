cecf0e52699ce588b8a5f5e4a4d47e86faf34521
Juan Diego Ramirez <jramir71@gmail.com>
2025-04-28 13:35:53 -0500

    se crea app usuarios, login, registro, logout

---
+++ usuarios/views.py
@@ -0,0 +1,35 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth import login as django_login 
+from django.contrib import messages
+
+def login(request):
+
+    if request.method == 'POST':
+        formulario = AuthenticationForm(request, data=request.POST)
+        if formulario.is_valid():
+            
+            usuario = formulario.get_user()
+
+            django_login(request, usuario)
+            messages.success(request, f'Bienvenido {usuario.username}')
+
+            return redirect('inicio')
+        
+    
+    else: 
+        formulario = AuthenticationForm()
+
+    return render(request, 'usuarios/login.html', {'formulario': formulario})
+
+def registro(request):
+    if request.method == 'POST':
+        formulario = UserCreationForm(request.POST)
+        if formulario.is_valid():
+            formulario.save()
+            messages.success(request, 'Usuario creado correctamente')
+            return redirect('login')
+    else:
+        formulario = UserCreationForm()
+    
+    return render(request, 'usuarios/registro.html', {'formulario': formulario})
